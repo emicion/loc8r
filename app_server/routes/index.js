9f07877da1d7ae0113643b953a12ee80899b01b2
Christopher Calderon <emicion@gmail.com>
2019-03-25 01:13:20 -0700

    Move folders. Define index controller.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+const homepageController = (req, res) => {
   res.render('index', { title: 'Express' });
-});
+};
+
+/* GET home page. */
+router.get('/', homepageController);
 
 module.exports = router;

c079ed30867b77339c582adcc737868965b79027
Christopher Calderon <emicion@gmail.com>
2019-03-25 18:12:19 -0700

    Add static placeholders with hardcoded data for clickable site.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,16 @@
 //Get 'Home' page
 const homelist = (req, res) => {
-  res.render('index', { title: 'Home'});
+  res.render('locations-list', { title: 'Home'});
 };
 
 //Get 'location Info' page
 const locationInfo = (req, res) => {
-  res.render('index', { title: 'Location Info' });
+  res.render('location-info', { title: 'Location Info' });
 };
 
 //Get 'Add Review' page
 const addReview = (req, res) => {
-  res.render('index', { title: 'Add Review' });
+  res.render('location-review-form', { title: 'Add Review' });
 };
 
 module.exports = {
